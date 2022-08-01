994cd53a4e581a058d067e4ae835389818aa919a
alexandre <alexandre.mahdhaoui@share-now.com>
2022-08-01 22:39:48 +0200

    feat(builder): implemented builder pattern

--- interfaces.go
+++ interfaces.go
@@ -1,6 +1,8 @@
 package tUtils
 
 import (
+	"crypto/tls"
+	"net/http"
 	"testing"
 )
 
@@ -10,4 +12,31 @@ type Tester interface {
 
 type Identifier interface {
 	Id() string
+}
+
+type HttpConfig interface {
+	HttpClient() *http.Client
+	Tester
+	TlsConfig() *tls.Config
+}
+
+type TlsTester interface {
+	Tester
+	TlsConfig() *tls.Config
+}
+
+type HttpTester interface {
+	HttpClient() *http.Client
+	Tester
+	TlsConfig() *tls.Config
+}
+
+type HttpConfigBuilder interface {
+	Build() HttpConfig
+
+	SetCookies(string, []*http.Cookie) HttpConfigBuilder
+	SetHttpClient(*http.Client) HttpConfigBuilder
+	SetProxy(string) HttpConfigBuilder
+	SetT(*testing.T) HttpConfigBuilder
+	SetTlsConfig(*tls.Config) HttpConfigBuilder
 }
